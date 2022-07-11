4d7404c875872a78abeffed81af36b821849cce7
ZoeDero <zoe.dero@gmail.com>
2022-07-11 13:13:28 +0200

    ca marche pas mais c'est a jour

--- js/reservation.js
+++ js/reservation.js
@@ -1,5 +1,9 @@
 document.querySelector("#buttonVerif").onclick = (event) => {
     event.preventDefault();
+
+    let disaButton=document.querySelector("#buttonVerif");
+         disaButton.classList.add("disabled");
+
     let nameInput = document.querySelector(".nameInput").value;
     let nameRegex = /(.*?)\s([\wáâàãçéêíóôõúüÁÂÀÃÇÉÊÍÓÔÕÚÜ]+\-?\'?\w+\.*?$)/;
     let resultName = nameRegex.test(nameInput);
@@ -15,24 +19,30 @@ document.querySelector("#buttonVerif").onclick = (event) => {
         document.querySelector("#eMailMsg").innerText = "E-mail invalide";
     }
     let telNum=document.querySelector(".telNum").value;
-    let telRegex = /^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))(Z|[\+-]\d{2}:\d{2})?)$/;
+    let telRegex = /^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/;
     let resultNum = telRegex.test(telNum);
     if (resultNum == false){
         document.querySelector("#telNumMsg").innerText = "Numéro invalide";
     }
     
     let entree=document.querySelector(".entree").value;
-    let entreeRegex = /^[^\d]+$/;
+    let entreeRegex = /^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))(Z|[\+-]\d{2}:\d{2})?)$/;
     let resultEntree = entreeRegex.test(entree);
     if (resultEntree == false){
         document.querySelector("#entreeMsg").innerText = "Date invalide";
     }
     
     let sortie=document.querySelector(".sortie").value;
-    let sortieRegex = /^[^\d]+$/;
+    let sortieRegex = /^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))(Z|[\+-]\d{2}:\d{2})?)$/;
     let resultSortie = sortieRegex.test(sortie);
     if (resultSortie == false){
         document.querySelector("#sortieMsg").innerText = "Date invalide";
     }
+
+    if (resultSortie == true && resultEntree == true && resultNum == true && resultEMail == true && resultName == true){
+            
+        disaButton.classList.remove("disabled");
+    }
+  
     
 }
