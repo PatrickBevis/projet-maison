b483e2d489ae5a6b216850868977eaf55a0d548e
ZoeDero <zoe.dero@gmail.com>
2022-07-11 10:34:08 +0200

    sldknfsdlk,

--- reservation.js
+++ reservation.js
@@ -1,25 +1,28 @@
-let nameInput=prompt(nameInput.value);
-let nameIsValid = /^[^\d]+$/;
-let resultName = nameIsValid.test(nameInput);
-if (nameIsValid = true){
-    alert("ok");
+document.querySelector(".btn").onclick = (event) => {
+    event.preventDefault();
+let nameInput=document.querySelector(".nameInput");
+let nameIsValid = /(.*?)\s([\wáâàãçéêíóôõúüÁÂÀÃÇÉÊÍÓÔÕÚÜ]+\-?\'?\w+\.*?$)/;
+let resultName = nameIsValid.test(nameInput).value;
+if (resultName == false){
+    document.querySelector("#nameMsg").innerText = "nom invalide";
 }
 
-let eMailInput=prompt(eMailInput.value);
-let eMailIsValid = /^[^\d]+$/;
-let resultEMail = nameIsValid.test(eMailIsValid);
+}
 
-let telNum=prompt(telNum.value);
-let numIsValid = /^[^\d]+$/;
-let resultNum = numIsValid.test(telNum);
+// let eMailInput=prompt(eMailInput.value);
+// let eMailIsValid = /^[^\d]+$/;
+// let resultEMail = nameIsValid.test(eMailIsValid);
 
-let entree=prompt(entree.value);
-let entreeIsValid = /^[^\d]+$/;
-let resultEntree = nameIsValid.test(entree);
+// let telNum=prompt(telNum.value);
+// let numIsValid = /^[^\d]+$/;
+// let resultNum = numIsValid.test(telNum);
 
-let sortie=prompt(sortie.value);
-let sortieIsValid = /^[^\d]+$/;
-let resultSormtie = nameIsValid.test(sortie);
+// let entree=prompt(entree.value);
+// let entreeIsValid = /^[^\d]+$/;
+// let resultEntree = nameIsValid.test(entree);
 
+// let sortie=prompt(sortie.value);
+// let sortieIsValid = /^[^\d]+$/;
+// let resultSormtie = nameIsValid.test(sortie);
 
 
