1644d7c4cc08d30b911e08c2ae00afd1e5352a9f
ZoeDero <zoe.dero@gmail.com>
2022-07-11 11:06:14 +0200

    ok

--- js/reservation.js
+++ js/reservation.js
@@ -1,25 +1,38 @@
-let nameInput=prompt(nameInput.value);
-let nameIsValid = /^[^\d]+$/;
-let resultName = nameIsValid.test(nameInput);
-if (nameIsValid = true){
-    alert("ok");
+document.querySelector("#buttonVerif").onclick = (event) => {
+    event.preventDefault();
+    let nameInput = document.querySelector(".nameInput").value;
+    let nameRegex = /(.*?)\s([\wáâàãçéêíóôõúüÁÂÀÃÇÉÊÍÓÔÕÚÜ]+\-?\'?\w+\.*?$)/;
+    let resultName = nameRegex.test(nameInput);
+    if (resultName == false){
+        document.querySelector("#nameMsg").innerText = "nom invalide";
+    }
+
+    
+    let eMailInput=document.querySelector(".eMailInput").value;
+    let eMailRegex = /^((\w[^\W]+)[\.\-]?){1,}\@(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let resultEMail = eMailRegex.test(eMailInput);
+    if (resultEMail == false){
+        document.querySelector("#eMailMsg").innerText = "E-mail invalide";
+    }
+    let telNum=document.querySelector(".telNum").value;
+    let telRegex = /^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))(Z|[\+-]\d{2}:\d{2})?)$/;
+    let resultNum = telRegex.test(telNum);
+    if (resultNum == false){
+        document.querySelector("#telNumMsg").innerText = "Numéro invalide";
+    }
+    
+    let entree=document.querySelector(".entree").value;
+    let entreeRegex = /^[^\d]+$/;
+    let resultEntree = entreeRegex.test(entree);
+    if (resultEntree == false){
+        document.querySelector("#entreeMsg").innerText = "Date invalide";
+    }
+    
+    let sortie=document.querySelector(".sortie").value;
+    let sortieRegex = /^[^\d]+$/;
+    let resultSortie = sortieRegex.test(sortie);
+    if (resultSortie == false){
+        document.querySelector("#sortieMsg").innerText = "Date invalide";
+    }
+    
 }
-
-let eMailInput=prompt(eMailInput.value);
-let eMailIsValid = /^[^\d]+$/;
-let resultEMail = nameIsValid.test(eMailIsValid);
-
-let telNum=prompt(telNum.value);
-let numIsValid = /^[^\d]+$/;
-let resultNum = numIsValid.test(telNum);
-
-let entree=prompt(entree.value);
-let entreeIsValid = /^[^\d]+$/;
-let resultEntree = nameIsValid.test(entree);
-
-let sortie=prompt(sortie.value);
-let sortieIsValid = /^[^\d]+$/;
-let resultSormtie = nameIsValid.test(sortie);
-
-
-
